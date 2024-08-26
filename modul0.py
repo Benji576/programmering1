6d7f86b0ac4ab8de98affe740d13c4bb6b5943c4
Benji576 <benjamin.wallfreyne@elev.ga.ntig.se>
2024-08-26 14:28:30 +0200

    uppgift1

---
+++ modul0.py
@@ -0,0 +1,31 @@
+#välkommen till python
+
+"""hello everyone
+detta är ett block av kommentarer """
+
+print("hello world")    #inom citattecken
+
+
+#string variabel
+name = "Benjamin"
+print(name)
+
+
+#inetger/ nummer variabel (heltal)
+my_age = 17
+print(my_age)
+
+print(name + " is " +  str(my_age) + " years old ")
+
+#float
+
+
+
+#boolean
+
+
+
+#ctrl + s för att spara
+
+
+
---
+++ modul1.py
@@ -0,0 +1,28 @@
+
+print("hello world")
+
+print("citat: \"Spiderman kommer att slå ned dig imorgon\"  ")
+
+
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+""")
+
+print("benjamin \nwall freyne")
+
+print("Fy" + "17!")
+
+name = input("Skriv namn: ")
+
+print("Välkommen \"" + name + "\"")
+
