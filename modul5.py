95923884a393134ae796351956efb8668e65fdc1
Benji576 <benjamin.wallfreyne@elev.ga.ntig.se>
2024-11-04 14:39:17 +0100

    project

---
+++ modul5.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls')
+
+#my_variable = "1002"
+
+#y = my_variable.isdigit()
+#print(y)
+
+
+my_number = "52"
+
+if my_number.isdigit():
+    print("Gissa mitt nummer(klicka på 'Q' om du vill avsluta programmet)")
