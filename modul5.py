918c5c31a247621ecb79f002a53593d291896036
Benji576 <benjamin.wallfreyne@elev.ga.ntig.se>
2024-11-27 12:42:15 +0100

    modul 5
    modul 5 2.0

--- modul5.py
+++ modul5.py
@@ -1,13 +1,58 @@
 import os
 os.system('cls')
 
-#my_variable = "1002"
+my_variable = "2"
 
-#y = my_variable.isdigit()
-#print(y)
+y = my_variable.isdigit()
+print(y)
 
 
-my_number = "52"
 
-if my_number.isdigit():
-    print("Gissa mitt nummer(klicka på 'Q' om du vill avsluta programmet)")
+while True:
+    try:  
+        numbers = int(input("Skriv siffror inget annat: "))
+    except:
+        print("Det är inte siffror")
+        continue
+
+    if number == int:
+        print(numbers)
+        break
+    
+
+
+while True:
+
+    word = str(input("Skriv ett ord och jag kan räkna mängden bokstäver innan yoghurten: "))
+    print(len(word))
+    
+
+    stop_program = str(input("Vill du avsluta så kan du klicka 'Q': ")).upper()
+
+    if stop_program in ['Q']:
+        print("Hejdå")
+        break
+    else:
+        continue
+
+os.system('cls')
+
+
+while True:
+
+    length_limit = 10
+
+    word = (input("Skriv ett ord och jag kommer kapa av det om du skriver för mycket"))
+    
+    if len(word) > length_limit:
+        word = print(word[:length_limit] + "...")
+        
+    stop_program = str(input("Vill du avsluta så kan du klicka 'Q': ")).upper()
+
+    if stop_program in ['Q']:
+        print("Hejdå")
+        break
+    else:
+        continue
+
+os.system('cls')
